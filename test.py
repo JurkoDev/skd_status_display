109943e626459f72cb86ca9c37affc37d97a4ab6
Jurko <50539591+JurkoDev@users.noreply.github.com>
2024-03-22 17:00:12 +0100

    idk

--- test.py
+++ test.py
@@ -124,7 +124,9 @@ async def youtube_dl_run(temp):
             if pin[i]["pin"] == temp["pin"]:
                 jsontemp["id"] = pin[i]["id"]
                 jsontemp["admin"] = pin[i]["admin"]
+            print("try:")
             print(pin[i]["id"])
+            print(pin[i]["pin"])
             print(temp["pin"])
             print(int(temp["pin"]))
 
